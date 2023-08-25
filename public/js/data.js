2318b57322bbaf8a54ef8d5cde846ed3a2167f01
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-25 18:15:42 +0530

    updated

--- public/js/data.js
+++ public/js/data.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const songs = [
+    {
+        name: 'Arcade',
+        path: '../assets/songs/arcade.mp3',
+        artist: 'Alan Walker',
+        cover: '../assets/images/cover/a.png'
+    },
+    
+]
