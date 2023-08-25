7b4cb1f259105ed6de587073a0fb067a97578910
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-25 15:17:38 +0530

    updated server

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname));
-
-const pathfile = path.join(__dirname, 'public');
-
-app.use(express.static(path.join(pathfile)));
-app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
